86c7ec05a8b9da9c062529e4861efc273684ff9b
Joey Kendall-Morwick <jbmorwick@gmail.com>
2024-11-06 22:25:50 -0600

    updated assignment

--- gradebook/GradeBook.java
+++ gradebook/GradeBook.java
@@ -6,47 +6,28 @@ public class GradeBook {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        students = new Student[] { new Student(), new Student() };
+        students[0].setFirstName("Alan");
+        students[1].setFirstName("Ada");
+        students[0].setLastName("Turing");
+        students[1].setLastName("Lovelace");
+        students[0].setGrade(95);
+        students[1].setGrade(87);
+
         System.out.println("Welcome to the CM111 Grade Book App!");
 
         while(true) {
             System.out.println("\nPlease make a selection:\n");
-            System.out.println("1) Load a gradebook from a file");
-            System.out.println("2) List Class Roster");
-            System.out.println("3) Enter Project Grade");
-            System.out.println("4) Enter Exam Grade");
-            System.out.println("5) Enter Lab Grade");
-            System.out.println("6) Enter Participation Score");
-            System.out.println("7) Print Overall Grades");
-            System.out.println("8) Save gradebook to a file");
-            System.out.println("9) Exit");
+            System.out.println("1) List Class Grades");
+            System.out.println("2) Exit");
             System.out.print("\nPlease choose an option: ");
             String choice = input.nextLine();
             System.out.println();
             switch(choice) {
-                case "1":
-                    System.out.println("Option not implemented.");
+                case "1": // TODO: write a loop that, for each student prints 
+                          //       lname, fname: grade 
                     continue;
                 case "2":
-                    continue;
-                case "3":
-                    System.out.println("Option not implemented.");
-                    continue;
-                case "4":
-                    System.out.println("Option not implemented.");
-                    continue;
-                case "5":
-                    System.out.println("Option not implemented.");
-                    continue;
-                case "6":
-                    System.out.println("Option not implemented.");
-                    continue;
-                case "7":
-                    System.out.println("Option not implemented.");
-                    continue;
-                case "8":
-                    System.out.println("Option not implemented.");
-                    continue;
-                case "9":
                     System.out.println("Goodbye!");
                     return;
             }
